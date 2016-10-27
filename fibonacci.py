94fa7357de40646b1d44a2ee78424b24f1eb89b3
gresendiz <gresendizc130@hotmail.com>
2016-10-26 20:17:46 -0500

    Test

---
+++ fibonacci.py
@@ -0,0 +1,13 @@
+
+#! /usr/bin/python
+#Autor: Guillermo Resendiz Chavez
+#Fecha: 24/Oct/2016
+#Descripcion: Serie fibonacci
+#Contacto: gresendizc130@hotmail.com
+
+#la suma de dos elemntos define el siguiente
+
+a, b = 0, 1
+while b < 10:
+	print(b)
+	a,b = b, a+b
